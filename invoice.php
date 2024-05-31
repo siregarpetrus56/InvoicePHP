1b58497934d7961f66642a84d1abb14b5a7dab92
unknown <siregarpetrus56@gmail.com>
2024-05-31 20:27:24 +0700

    first commit

---
+++ invoice.php
@@ -0,0 +1,163 @@
+<HTML>
+   <HEAD>
+   	  <TITLE>LOGIKA</TITLE>
+   </HEAD>
+   <BODY bgcolor=whitesmoke>
+		<table width=100% cellpadding=0 cellspacing=0 border=0>
+			<tr>
+				<td width=35% align=center valign=center bgcolor=lightgrey>
+					<img src="companylogo.PNG" width=120 height=100>
+				</td>
+				<td rowspan="2"><h1><font face=Verdana>INVOICE</h1></font>
+					<font face=Verdana><b>Date: </b>February 12,2020</font><BR>
+					<font face=Verdana><b>Invoice No: </b>003</font>
+					<br><br>
+						<table width=100% height=10px cellpadding=10 cellspacing=0 border=0 bgcolor=white>
+							<tr>
+								<th width=20%  align=justify>
+									<font face=Verdana   size=3>ITEM</font>
+								</th>
+								<th width=20%  align=center valign=center>
+									<font face=Verdana   size=3>QTY</font>
+								</th>
+								<th width=20%  align=center valign=center>
+									<font face=Verdana   size=3>PRICE</font>
+								</th>
+								<th width=20%  align=center valign=center>
+									<font face=Verdana   size=3>TOTAL</font>
+								</th>
+							</tr>
+						</table>
+						<table width=100% height=10% cellpadding=10 cellspacing=0 border=0>
+							<tr>
+								<td width=20% height=10% align=justify valign=center>
+									<font face=Verdana   size=3>Flyer Design</font>
+								</td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>02</font>
+								</td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>$300.00</font>
+								</td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>$600.00</font>
+								</td>
+							</tr>
+							<tr>
+								<td width=20% height=10% align=justify valign=center>
+									<font face=Verdana   size=3>Business Card Design</font>
+								</td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>02</font>
+								</td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>$200.00</font>
+								</td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>$400.00</font>
+								</td>
+							</tr>
+							<tr>
+								<td width=20% height=10% align=justify valign=center>
+									<font face=Verdana   size=3>Invoice Design</font>
+								</td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>02</font>
+								</td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>$250.00</font>
+								</td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>$500.00</font>
+								</td>
+							</tr>
+							<tr>
+								<td width=20% height=10% align=justify valign=center>
+									<font face=Verdana   size=3>Logo Design</font>
+								</td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>01</font>
+								</td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>$500.00</font>
+								</td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>$500.00</font>
+								</td>
+							</tr>
+							<tr>
+								<td width=20% height=10% align=justify valign=center>
+									<font face=Verdana   size=3>Web Design</font></td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>01</font></td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>$500.00</font></td>
+								<td width=20%  align=center valign=center>
+									<font face=Verdana   size=3>$500.00</font></td>
+							</tr>
+						</table>
+						<table width=100% cellpadding=1 border=0 bgcolor=pink>
+							<tr>
+								<td></td>
+							</tr>
+						</table>
+						<br><br>
+						<table width=100% height=10% cellpadding=10 cellspacing=0 border=0>
+							<tr>
+								<td width=70% align=right>
+									<font face=Verdana  size=3>SUBTOTAL</font>
+								</td>
+								<td width=30% align=center>
+									<font face=Verdana  size=3>$2,500.00</font>
+								</td>
+							</tr>
+							<tr>
+								<td align=right>
+									<font face=Verdana size=3>Tax (15%)</font>
+								</td>
+								<td align=center>
+									<font face=Verdana  size=3>$374</font>
+								</td>
+							</tr>
+							<tr>
+								<td align=right>
+									<font face=Verdana  size=3>GRAND TOTAL</font>
+								</td>
+								<td align=center>
+									<font face=Verdana  size=3>$2,874.38</font>
+								</td>
+							</tr>
+						</table>
+					<br><br><br><br><br>
+					<font face=Verdana><b>Questions?</b></font><BR>
+					<font face=Verdana>Email us at accounts@makestudios.com</font><BR>
+					<font face=Verdana>or call us at 1-917-594-7788</font><BR>
+				</td>
+			</tr>
+			<tr>
+				<td height=50% valign=top bgcolor=pink>
+					<font face=Verdana align=center size=50><b>Ethan Meyer<br><br>
+						<table width=40% cellpadding=1 border=0 bgcolor=black>
+							<tr>
+								<td></td>
+							</tr>
+						</table><br>
+					<font face=Verdana align=center size=3>Maggie's Clothing Co.<br>
+					<font face=Verdana align=center size=3>508-572-1812<br>
+					<font face=Verdana align=center size=3>2211 Kovar Road,<br>
+					<font face=Verdana align=center size=3>Burlington, MA 01803<br><br><br><br><br>
+					<font face=Verdana align=center size=3><b>PAYMENT METHOD<br><br>
+					<font face=Verdana align=center size=3><b>ACCOUNT NUMBER:<br>
+					<font face=Verdana align=center size=3><b>4916 8418 9082 4339<br><br>
+					<font face=Verdana align=center size=3><b>EMAIL ADDRESS:<br>
+					<font face=Verdana align=center size=3><b>andreaclothing@mail.com<br>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2" align=center bgcolor=lightgrey>
+					<font face=CourierNew color=white size=5>4102 Gilmore 	Avenue, New York, NY 10016 . makestudios.com</font>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
